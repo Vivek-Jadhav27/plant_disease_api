4d4ac5b50d8760b3442fbdf1348684c6cc7111c5
Vivek Jadhav <vcjadhav27@gmail.com>
2025-03-11 13:37:47 +0530

    Completed

--- detection/urls.py
+++ detection/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, predict , predict_api
 
 urlpatterns = [
-    path('home/', home, name="home"),
+    path('', home, name="home"),
     path('predict/', predict, name="predict"),
     path('predict_api/',predict_api,name="predict_api"),
 ]
